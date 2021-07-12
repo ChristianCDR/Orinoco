24e05e545113e473f80787388d5520bd0039fd72
Christian CLAUDE DIT REGENT <christian.cdr@outlook.fr>
2021-07-12 13:14:40 +0200

    Page produit dynamique

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
-fetch("http://localhost:3000/api/cameras")
-  .then(function(resultat){
+fetch("http://localhost:3000/api/cameras") //requête à l'api
+  .then(function(resultat){ //un .then pour récuperer le résultat
     if (resultat.ok){
-      return resultat.json();
+      return resultat.json(); //on récupère le resultat que l'on convertit au format json
     }
   })
-  .then (function(valeur){
-    let element = document.querySelector('.big-row');
+  .then (function(valeur){ //un 2e .then pour récuperer le résultat au format json
+    let element = document.querySelector('.big-row'); 
     for(let i=0; i< valeur.length; i++ ){
-      let camera_actuelle = valeur[i];
-      let price=camera_actuelle.price/100;
+      let camera_actuelle = valeur[i]; // camera_actuelle contiendra tour à tour les attributs de chaque caméra
+      let price=camera_actuelle.price/100; // conversion des centimes en dollars
       //console.log(camera_actuelle);
       let newItem= document.createElement('div');
          newItem.classList.add("col-12", "col-md-6","col-lg-4");
          newItem.innerHTML=`<div class="card bg-secondary div__img">
          <div>
-           <a class="stretched-link" href="html/page-produit.html">
+           <a class="stretched-link" href="html/page-produit.html?${camera_actuelle._id}">
             <img class="card-img-top" src="${camera_actuelle.imageUrl}" alt="Image de l'appareil photo ">
            </a>
          </div>
@@ -25,11 +25,36 @@ fetch("http://localhost:3000/api/cameras")
          </div>
          <p>${camera_actuelle.description}</p>
        </div>`;
-       element.appendChild(newItem);
+       element.appendChild(newItem); // nouvel enfant à .big-row à chaque boucle
     }
 
   })
   .catch(function(error){
     console.log(error);
   })
+  let url_produit = (window.location.search).slice(1);
+  console.log(url_produit);
+  fetch(`http://localhost:3000/api/cameras/${url_produit}`)
+  .then(function(secondResultat){
+    if (secondResultat.ok){
+      return secondResultat.json();
+    }
+  })
+  .then(function(secondValeur){
+    console.log(secondValeur);
+    let image_parent = document.querySelector('.img-parent');
+    let image= document.createElement('div');
+      image.innerHTML=`<img class="card-img-top rounded" src="${secondValeur.imageUrl}" alt="photo de la caméra" >`;
+      console.log(image.innerHTML);
+      image_parent.prepend(image);
+    let image_title = document.querySelector('.card-title');
+    console.log(image_title);
+    image_title.innerHTML=`${secondValeur.name}`;
+
+
+    })
+  .catch(function(secondError){
+    console.log(secondError);
+  })
+
   
