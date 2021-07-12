89ad8b5ef1833955a728fe54659b96a3f3a702c3
Christian CLAUDE DIT REGENT <christian.cdr@outlook.fr>
2021-07-13 00:33:58 +0200

    same

--- js/index.js
+++ js/index.js
@@ -53,6 +53,8 @@ fetch("http://localhost:3000/api/cameras") //requête à l'api
     let lens= document.querySelector('.lens');
     let firstlens = document.createElement('div');
     let secondlens= document.createElement('div');
+    let lens_class1=0;
+    let lens_class2=0;
 
 switch(id_produit){
   case "5be1ed3f1c9d44000030b061":
@@ -61,44 +63,94 @@ switch(id_produit){
         lens.appendChild(firstlens);
         lens.appendChild(secondlens);
 
-        let lens_class1= document.querySelector(".lentille1");
-            lens_class1.addEventListener('click', function(){
-            lens_class1.style.border="medium solid blue";
-            console.log(secondValeur.lenses[0]);
-
+        lens_class1= document.querySelector(".lentille1");
+        lens_class1.addEventListener('click', function(){
+        lens_class1.style.border="medium solid blue";
+        console.log(secondValeur.lenses[0]);
         });
+
         lens_class2= document.querySelector(".lentille2");
         lens_class2.addEventListener('click', function(){
-          lens_class2.style.border="medium solid blue";
-          console.log(secondValeur.lenses[1]);
+        lens_class2.style.border="medium solid blue";
+        console.log(secondValeur.lenses[1]);
         })
-
   break;
   case "5be1ef211c9d44000030b062":
-        firstlens.innerHTML=`<img src="../images/Lens_35mm_1.4.jpg" alt="Lentille" >`;
-        secondlens.innerHTML=`<img src="../images/Lens_50mm_1.6.jpg" alt="Lentille" >`;
+        firstlens.innerHTML=`<img class="lentille1" src="../images/Lens_35mm_1.4.jpg" alt="Lentille">`;
+        secondlens.innerHTML=`<img class="lentille2" src="../images/Lens_50mm_1.6.jpg" alt="Lentille">`;
         lens.appendChild(firstlens);
         lens.appendChild(secondlens);
+
+        lens_class1= document.querySelector(".lentille1");
+        lens_class1.addEventListener('click', function(){
+        lens_class1.style.border="medium solid blue";
+        console.log(secondValeur.lenses[0]);
+        });
+
+        lens_class2= document.querySelector(".lentille2");
+        lens_class2.addEventListener('click', function(){
+        lens_class2.style.border="medium solid blue";
+        console.log(secondValeur.lenses[1]);
+        })
   break;
   case "5be9bc241c9d440000a730e7":
-        firstlens.innerHTML=`<img src="../images/Lens_35mm_1.4.jpg" alt="Lentille" >`;
-        secondlens.innerHTML=`<img src="../images/Lens_50mm_1.6.jpg" alt="Lentille" >`;
+        firstlens.innerHTML=`<img class="lentille1" src="../images/Lens_35mm_1.4.jpg" alt="Lentille">`;
+        secondlens.innerHTML=`<img class="lentille2" src="../images/Lens_50mm_1.6.jpg" alt="Lentille">`;
         lens.appendChild(firstlens);
         lens.appendChild(secondlens);
+
+        lens_class1= document.querySelector(".lentille1");
+        lens_class1.addEventListener('click', function(){
+        lens_class1.style.border="medium solid blue";
+        console.log(secondValeur.lenses[0]);
+        });
+
+        lens_class2= document.querySelector(".lentille2");
+        lens_class2.addEventListener('click', function(){
+        lens_class2.style.border="medium solid blue";
+        console.log(secondValeur.lenses[1]);
+        })
   break;
   case "5be9c4471c9d440000a730e8":
-        firstlens.innerHTML=`<img src="../images/Lens_35mm_1.4.jpg" alt="Lentille" >`;
-        secondlens.innerHTML=`<img src="../images/Lens_50mm_1.6.jpg" alt="Lentille" >`;
+        firstlens.innerHTML=`<img class="lentille1" src="../images/Lens_35mm_1.4.jpg" alt="Lentille">`;
+        secondlens.innerHTML=`<img class="lentille2" src="../images/Lens_50mm_1.6.jpg" alt="Lentille">`;
         lens.appendChild(firstlens);
         lens.appendChild(secondlens);
+
+        lens_class1= document.querySelector(".lentille1");
+        lens_class1.addEventListener('click', function(){
+        lens_class1.style.border="medium solid blue";
+        console.log(secondValeur.lenses[0]);
+        });
+
+        lens_class2= document.querySelector(".lentille2");
+        lens_class2.addEventListener('click', function(){
+        lens_class2.style.border="medium solid blue";
+        console.log(secondValeur.lenses[1]);
+        })
   break;
   case "5be9c4c71c9d440000a730e9":
-        firstlens.innerHTML=`<img src="../images/Lens_35mm_1.4.jpg" alt="Lentille" >`;
-        secondlens.innerHTML=`<img src="../images/Lens_50mm_1.6.jpg" alt="Lentille" >`;
+        firstlens.innerHTML=`<img class="lentille1" src="../images/Lens_35mm_1.4.jpg" alt="Lentille">`;
+        secondlens.innerHTML=`<img class="lentille2" src="../images/Lens_50mm_1.6.jpg" alt="Lentille">`;
         lens.appendChild(firstlens);
         lens.appendChild(secondlens);
+
+        lens_class1= document.querySelector(".lentille1");
+        lens_class1.addEventListener('click', function(){
+        lens_class1.style.border="medium solid blue";
+        console.log(secondValeur.lenses[0]);
+        });
+
+        lens_class2= document.querySelector(".lentille2");
+        lens_class2.addEventListener('click', function(){
+        lens_class2.style.border="medium solid blue";
+        console.log(secondValeur.lenses[1]);
+        })
+  
   default: console.log("Pas de lentilles disponibles");
 }
+let link= document.querySelector('a.btn');
+    link.innerHTML=`<a role="button" class="btn rounded bg-info text-white" href="panier.html?${id_produit}&${secondValeur.lenses[0]}"> Ajouter au panier </a>`;
 })
 .catch(function(secondError){
   console.log(secondError);
