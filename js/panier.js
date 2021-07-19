09d96e79a5b35a84d4fd518b7746b1410cb1c0b1
Christian CLAUDE DIT REGENT <christian.cdr@outlook.fr>
2021-07-19 23:50:26 +0200

    form corrigé

--- js/panier.js
+++ js/panier.js
@@ -1,5 +1,4 @@
 let recapCommande= JSON.parse(localStorage.getItem("selectedItemTab"));
-console.log(recapCommande);
 let nomProduit= document.querySelector('.produit');
 let total=document.querySelector('.total');
 
@@ -33,38 +32,34 @@ let recapTab=document.querySelector('table');
   }
 
 let request= document.querySelectorAll('input');
+let contact={};
+    for(let i=0; i<request.length; i++){
+      nomDeFamille=request[0].value;
+      prenom=request[1].value;
+      email=request[2].value;
+      adresse=request[3].value;
+      codePostal=request[4].value;
+      ville=request[5].value;
+    }
+    contact={
+      lastName:`${nomDeFamille}`,
+      firstName:`${prenom}`,
+      email:`${email}`,
+      address:`${adresse}`,
+      codePostal:`${codePostal}`,
+      city:`${ville}`
+    }
 
 let codePostalCheck= document.getElementsByName('codePostal');
     codePostalCheck[0].addEventListener('change',function(value){
        let codePostal= localStorage.setItem('codePostal', JSON.stringify(value.target.value));
     });
 
-let contact={};
-    for(let i=0 ; i<request.length; i++){
-        contact={
-        lastName:`${request[0].value}`,
-        firstName:`${request[1].value}`,
-        email:`${request[2].value}`,
-        address:`${request[3].value}`,
-        codePostal:`${request[4].value}`,
-        city:`${request[5].value}`
-      }
-    }
-
     let products= JSON.parse(localStorage.getItem("productIdTab"));
 
 let submitButton=document.querySelector('.submit');
     submitButton.addEventListener('click',function(event){
-      let codePostal= JSON.parse(localStorage.getItem("codePostal"));
-      if(/^[0-9]{5}$/.test(codePostal)){
-        request[4].value=codePostal;
-        console.log('valeur changée');
-      } 
-      else{
-        alert('Le code postal doit être composé de 5 chiffres!');
-        event.preventDefault();
-      }
-
+      
       fetch('http://localhost:3000/api/cameras/order', {
         method: 'POST',
         headers: { 
@@ -83,6 +78,15 @@ let submitButton=document.querySelector('.submit');
       .catch(error=>{
         console.log(error);
       })
+
+      let codePostal= JSON.parse(localStorage.getItem("codePostal"));
+      if(/^[0-9]{5}$/.test(codePostal)){
+        request[4].value=codePostal;
+      } 
+      else{
+        alert('Le code postal doit être composé de 5 chiffres!');
+        event.preventDefault();
+      }
     });
 
 // correction de la mise de la mise en page, pwp, plan de test/commentaires
